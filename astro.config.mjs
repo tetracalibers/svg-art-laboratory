b95229648c51c0e9d6297771c50938367163993a
Kanno Ami (tomixy) <tomix05may@gmail.com>
2024-03-28 05:42:16 +0900

    pathエイリアス設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+
+// for path aliases
+import { resolve } from 'node:path'
+const __dirname = new URL('.', import.meta.url).pathname
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  /** @type {import('astro').AstroUserConfig} */
+  vite: {
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
+      },
+    },
+  },
+})
