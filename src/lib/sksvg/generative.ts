7c35e5b0e956644d32ef04f19dfb85683f4f9e02
Kanno Ami (tomixy) <tomix05may@gmail.com>
2024-03-29 21:14:32 +0900

    change関数の追加

--- src/lib/sksvg/generative.ts
+++ src/lib/sksvg/generative.ts
@@ -13,3 +13,10 @@ export function random(min = 0, max = 1, float = false): number {
 export const pickRandom = <Item>(items: Item[]): Item => {
   return items[Math.round(Math.random() * (items.length - 1))]
 }
+
+// n2が未指定の場合 => n1%の確率でtrueを返す
+// n2を指定した場合 => n1 : n2 の確率でtrueを返す
+export const chance = (n1 = 50, n2?: number) => {
+  let n = n2 !== undefined ? (n2 / (n1 + n2)) * 100 : n1
+  return n > Math.random() * 100
+}
