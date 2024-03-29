5905e991ba4f2af914f96703eec46ff6724bdba6
Kanno Ami (tomixy) <tomix05may@gmail.com>
2024-03-29 21:14:58 +0900

    random関数からpickRandom関数に切り出した処理を削除

--- src/lib/sksvg/generative.ts
+++ src/lib/sksvg/generative.ts
@@ -1,13 +1,7 @@
 export function random(min = 0, max = 1, float = false): number {
-  if (Array.isArray(arguments[0])) {
-    // picks a random item from an array
-    const arr = arguments[0]
-    return arr[Math.round(Math.random() * (arr.length - 1))]
-  } else {
-    // gets a random number between a minimum and maximum value
-    const random = Math.random() * (max - min) + min
-    return float || max - min <= 1 ? random : Math.round(random)
-  }
+  // gets a random number between a minimum and maximum value
+  const random = Math.random() * (max - min) + min
+  return float || max - min <= 1 ? random : Math.round(random)
 }
 
 export const pickRandom = <Item>(items: Item[]): Item => {
