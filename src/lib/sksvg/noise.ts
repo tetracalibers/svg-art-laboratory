64903b328cafcd99f01e8c6fcfdebdc9212e0aa1
Kanno Ami (tomixy) <tomix05may@gmail.com>
2024-03-30 07:47:45 +0900

    PerlinNoise2Dが正しい結果を返さない様子のため、書き直し

--- src/lib/sksvg/noise.ts
+++ src/lib/sksvg/noise.ts
@@ -1,17 +1,18 @@
 export class PerlinNoise2D {
-  cache: Map<[number, number], number>
-  grad: Map<[number, number], { x: number; y: number }>
+  private grad: { [key: string]: { x: number; y: number } }
+  private cache: { [key: string]: number }
 
   constructor() {
-    this.cache = new Map()
-    this.grad = new Map()
+    this.grad = {}
+    this.cache = {}
   }
 
-  // Get the noise value at the specified co-ordinates.
-  // - accepts an x coordinate and optional y coordinate as arguments and returns the value at that point in the noise space
-  get(x: number, y = 0) {
-    const cached = this.cache.get([x, y])
-    if (cached) return cached
+  get(x: number, y: number = 0): number {
+    const key = `${x},${y}`
+
+    if (key in this.cache) {
+      return this.cache[key]
+    }
 
     const xf = Math.floor(x)
     const yf = Math.floor(y)
@@ -25,30 +26,31 @@ export class PerlinNoise2D {
     const xb = this.#fade(x - xf, bl, br)
     const v = this.#fade(y - yf, xt, xb)
 
-    this.cache.set([x, y], v)
+    this.cache[key] = v
 
     return v
   }
 
-  #gridDotProduct(x: number, y: number, vx: number, vy: number) {
+  #gridDotProduct(x: number, y: number, vx: number, vy: number): number {
+    const key = `${vx},${vy}`
     let gVec: { x: number; y: number }
-    const dVec = { x: x - vx, y: y - vy }
+    let dVec = { x: x - vx, y: y - vy }
 
-    const prev = this.grad.get([vx, vy])
-
-    if (prev) {
-      gVec = prev
+    if (key in this.grad) {
+      gVec = this.grad[key]
     } else {
-      let th = Math.random() * 2 * Math.PI
+      const th = Math.random() * 2 * Math.PI
       gVec = { x: Math.cos(th), y: Math.sin(th) }
-      this.grad.set([vx, vy], gVec)
+      this.grad[key] = gVec
     }
 
     return dVec.x * gVec.x + dVec.y * gVec.y
   }
 
-  #fade(x: number, a: number, b: number) {
-    let s = 6 * x ** 5 - 15 * x ** 4 + 10 * x ** 3
-    return a + s * (b - a)
+  #fade(t: number, a: number, b: number): number {
+    const t3 = t * t * t
+    const t4 = t3 * t
+    const t5 = t4 * t
+    return a + (6 * t5 - 15 * t4 + 10 * t3) * (b - a)
   }
 }
