cdc530e1c7cfb133c8ebe79f8be297b50b08fa47
Kanno Ami (tomixy) <tomix05may@gmail.com>
2024-04-07 21:41:39 +0900

    アルキメデス螺旋

---
+++ src/sketches/archimedes-spiral.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+  import { SkSVG } from '@/lib/sksvg'
+  import { onMount } from 'svelte'
+
+  let container: HTMLElement
+
+  let winInnerWidth: number
+  let winInnerHeight: number
+
+  // 曲線の精度
+  const CURVE_STEP = 2 * Math.PI * 0.01
+
+  // -- アルキメデス螺旋 ---
+  onMount(() => {
+    // --- settings ---
+
+    // viewport size (1:1 aspect ratio).
+    const svgSize = Math.min(winInnerWidth, winInnerHeight)
+    // viewbox size
+    const canvasSize = 500
+
+    // アルキメデス螺旋の動径を計算する関数
+    const radius = (t: number) => 2 * t
+
+    // --- initialize elements ---
+
+    const svg = new SkSVG()
+    svg.addTo(container)
+    svg.set({
+      viewBox: `0 0 ${canvasSize} ${canvasSize}`,
+      width: svgSize,
+      height: svgSize,
+    })
+
+    const group = svg.create('g')
+    group.moveTo(canvasSize / 2, canvasSize / 2)
+
+    // --- main process ---
+
+    const points: number[] = []
+
+    for (let t = 0; t < 100; t += CURVE_STEP) {
+      const r = radius(t)
+      const x = r * Math.cos(t)
+      const y = r * Math.sin(t)
+      points.push(x, y)
+    }
+
+    group.createCurve(points).set({
+      stroke: '#D2E0FB',
+    })
+  })
+</script>
+
+<svelte:window
+  bind:innerWidth="{winInnerWidth}"
+  bind:innerHeight="{winInnerHeight}"
+/>
+
+<div bind:this="{container}" class="sketch-container"></div>
+
+<style>
+  .sketch-container {
+    display: flex;
+  }
+</style>
